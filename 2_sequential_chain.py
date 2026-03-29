e1535cde229db653387b229f35c6d60f88922ad9
Vipul Kumar Singh <120162007+VIPULSINGH18@users.noreply.github.com>
2026-03-30 00:43:40 +0530

    Add files via upload

---
+++ 2_sequential_chain.py
@@ -0,0 +1,30 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Generate a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a 5 pointer summary from the following text \n {text}',
+    input_variables=['text']
+)
+
+model = ChatGroq(
+    model="llama-3.3-70b-versatile",  # Best for RAG (Balance of speed/intelligence)
+    temperature=0.2,                    # 0 is best for factual RAG responses
+    max_retries=2,
+)
+
+parser = StrOutputParser()
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+result = chain.invoke({'topic': 'Unemployment in India'})
+
+print(result)
